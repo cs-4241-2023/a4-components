53ad73d024a2f3288ce0dece13da8abc53147cd9
SamKarkache <93103617+SamKarkache@users.noreply.github.com>
2023-09-27 12:48:44 -0400

    rough draft of datapage

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function handleClick(event) {
+        event.preventDefault();
+        alert("Hello World")
+    }
+
+        return (
+            <>
+                <header>
+                    <h1>WPI School Work Tracker</h1>
+                    <p>
+                        This application allows you to track assignments for your WPI classes. Please input information about the assignment
+                        below and this web application will store it for you! The application will also calculate a relative priority for the
+                        assignment based on the difficulty and due date of the inputted assignment.
+                    </p>
+                </header>
+
+                <form id={"assignment-form"} autoComplete={"off"}>
+                    <input aria-label={"class-name"} type={"text"} id={"class-name"} placeholder={"Class Name"}/>
+                    <input aria-label={"assignment-name"} type={"text"} id={"assignment-name"} placeholder={"Assignment Name"}/>
+                    <input aria-label={"due-date"} type={"date"} id={"due-date"} />
+                    <input aria-label={"difficulty"} type={"number"} id={"difficulty"} placeholder={"Difficulty (1 to 10)"}/>
+                    <label form={"assignment-form"}>Completed? <input aria-label={"completed"} type={"checkbox"} id={"completed"}/></label>
+                    <button id={"submit-button"} onClick={handleClick}>Submit</button>
+                </form>
+            </>
+        );
 }
-
 export default App
