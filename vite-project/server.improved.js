a2b92471281d42d6b8adc005b880567a7fe081c2
Sombero <73486390+Sombero@users.noreply.github.com>
2023-10-02 18:29:18 -0400

    Changed from HTTP -> Vite-express.

--- vite-project/server.improved.js
+++ vite-project/server.improved.js
@@ -1,8 +1,8 @@
-import http from 'http';
-import fs from 'fs';
-import mime from 'mime';
+import express from "express";
+import ViteExpress from "vite-express";
 
-let port = 3000
+const app = express();
+
 
 const appdata = [
   {'yourname': 'Justin', 'username': 'Sombero', 'email': 'jwonoski2@wpi.edu', 'position': 'DPS'},
@@ -10,114 +10,135 @@ const appdata = [
   {'yourname': 'Tim', 'username': 'Robo', 'email': 'tRobo@wpi.edu', 'position': 'Tank'} 
 ]
 
-const headers = {
-  'Access-Control-Allow-Origin': '*',  
-  'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, DELETE, SUBMIT, EDIT',
-  'Access-Control-Allow-Headers': 'Content-Type',
-};
+// const headers = {
+//   'Access-Control-Allow-Origin': '*',  
+//   'Access-Control-Allow-Methods': 'OPTIONS, POST, GET, DELETE, SUBMIT, EDIT',
+//   'Access-Control-Allow-Headers': 'Content-Type',
+// };
 
-const server = http.createServer( function( request,response ) {
-  if (request.method === 'OPTIONS') {
-    response.writeHead(204, headers);
-    response.end();
-    return;
-  }
-  if( request.method === 'GET' ) {
-    handleGet( request, response )    
-  }else if( request.method === 'POST' ){
-    handlePost( request, response ) 
-  } else if( request.method === 'DELETE' ){
-  handlePost( request, response )
-} else if (request.method === 'EDIT'){
-  handlePost( request, response )
-}
+// const server = http.createServer( function( request,response ) {
+//   if (request.method === 'OPTIONS') {
+//     response.writeHead(204, headers);
+//     response.end();
+//     return;
+//   }
+//   if( request.method === 'GET' ) {
+//     handleGet( request, response )    
+//   }else if( request.method === 'POST' ){
+//     handlePost( request, response ) 
+//   } else if( request.method === 'DELETE' ){
+//   handlePost( request, response )
+// } else if (request.method === 'EDIT'){
+//   handlePost( request, response )
+// }
+// })
+
+
+
+app.get( '/get', ( req, res ) => {
+  res.status( 200 ).json(appdata)
 })
 
-const handleGet = function( request, response ) {
-  if( request.url === '/get' ) {
-    console.log('fetch data')
-    response.writeHead( 200, "OK", {...headers, 'Content-Type': 'text/plain' })
-    response.end(JSON.stringify(appdata))
-  }
-}
+app.post( '/submit', ( req, res ) => {
+  // our request object now has a 'json' field in it from our previous middleware
+  console.log( req.body )
+  appdata.push( req.body )  
+  res.writeHead( 200, { 'Content-Type': 'application/json'})
+  res.end( req.json )
+})
 
+app.delete( '/delete', ( req, res ) => {
+    const pee = req.body
+    const num = Number(pee.index)
+    console.log(num)
+      appdata.splice(num, 1)
+  res.status( 200 ).json(appdata)
+})
 
-const handlePost = function( request, response ) {
-  let dataString = ''
+app.put( '/edit', ( req, res ) => {
+  const playerinfo = req.body
+  const index = playerinfo.index
+    appdata[index] = playerinfo.playerdata
+  res.status( 200 ).json(appdata)
+});
 
-  if( request.url === '/delete' ) {
-    request.on( 'data', function( data ) {
-      dataString += data
+// const handlePost = function( request, response ) {
+//   let dataString = ''
+
+//   if( request.url === '/delete' ) {
+//     request.on( 'data', function( data ) {
+//       dataString += data
       
-      console.log(JSON.parse( dataString ))
-      //This deserves to be pee. Don't debate with me‹.
-      const pee = JSON.parse( dataString )  
-      const num = Number(pee.index)
-      console.log(num)
-        appdata.splice(num, 1)
-    })
+//       console.log(JSON.parse( dataString ))
+//       //This deserves to be pee. Don't debate with me‹.
+//       const pee = JSON.parse( dataString )  
+//       const num = Number(pee.index)
+//       console.log(num)
+//         appdata.splice(num, 1)
+//     })
 
-    request.on( 'end', function() {
-      response.writeHead( 200, "OK", {...headers, 'Content-Type': 'text/plain' })
-      response.end('test')
-    })
-  }  
-  // Implementation for Edit: If I got time. 
-  else if( request.url === '/edit' ) {
-    //Implement edit function here.
-    request.on( 'data', function( data ) {
-      //Data.playerData is being sent from main.js 
-      dataString += data //Adding data to assembly code to make it a string.
-      const playerinfo = JSON.parse(dataString)
-      const index = playerinfo.index
-      console.log(dataString)
-      console.log(JSON.parse(dataString))
-        appdata[index] = playerinfo.playerdata
-    })
+//     request.on( 'end', function() {
+//       response.writeHead( 200, "OK", {...headers, 'Content-Type': 'text/plain' })
+//       response.end('test')
+//     })
+//   }  
+//   // Implementation for Edit: If I got time. 
+//   else if( request.url === '/edit' ) {
+//     //Implement edit function here.
+//     request.on( 'data', function( data ) {
+//       //Data.playerData is being sent from main.js 
+//       dataString += data //Adding data to assembly code to make it a string.
+//       const playerinfo = JSON.parse(dataString)
+//       const index = playerinfo.index
+//       console.log(dataString)
+//       console.log(JSON.parse(dataString))
+//         appdata[index] = playerinfo.playerdata
+//     })
 
-    request.on( 'end', function() {
-      response.writeHead( 200, "OK", {...headers, 'Content-Type': 'text/plain' })
-      response.end('test')
-    })
-  }
-  else {
-    request.on( 'data', function( data ) {
-      dataString += data 
-      console.log(dataString)
-  })
+//     request.on( 'end', function() {
+//       response.writeHead( 200, "OK", {...headers, 'Content-Type': 'text/plain' })
+//       response.end('test')
+//     })
+//   }
+//   else {
+//     request.on( 'data', function( data ) {
+//       dataString += data 
+//       console.log(dataString)
+//   })
 
-    request.on( 'end', function() {
-    console.log( JSON.parse( dataString ) )
+//     request.on( 'end', function() {
+//     console.log( JSON.parse( dataString ) )
 
-    // ... do something with the data here!!!
-    appdata.push( JSON.parse( dataString ) )
-    response.writeHead( 200, "OK", {...headers, 'Content-Type': 'text/plain' })
-    response.end('test')
-  })
-  }
+//     // ... do something with the data here!!!
+//     appdata.push( JSON.parse( dataString ) )
+//     response.writeHead( 200, "OK", {...headers, 'Content-Type': 'text/plain' })
+//     response.end('test')
+//   })
+//   }
 
-}
+// }
 
-const sendFile = function( response, filename ) {
-   const type = mime.getType( filename ) 
+// const sendFile = function( response, filename ) {
+//    const type = mime.getType( filename ) 
 
-   fs.readFile( filename, function( err, content ) {
+//    fs.readFile( filename, function( err, content ) {
 
-     // if the error = null, then we've loaded the file successfully
-     if( err === null ) {
+//      // if the error = null, then we've loaded the file successfully
+//      if( err === null ) {
 
-       // status code: https://httpstatuses.com
-       response.writeHeader( 200, { 'Content-Type': type })
-       response.end( content )
+//        // status code: https://httpstatuses.com
+//        response.writeHeader( 200, { 'Content-Type': type })
+//        response.end( content )
 
-     }else{
+//      }else{
 
-       // file not found, error code 404
-       response.writeHeader( 404 )
-       response.end( '404 Error: File Not Found' )
+//        // file not found, error code 404
+//        response.writeHeader( 404 )
+//        response.end( '404 Error: File Not Found' )
 
-     }
-   })
-}
+//      }
+//    })
+// }
 
-server.listen( process.env.PORT || port )
+const server = app.listen(3000, "0.0.0.0", () => console.log("Server is listening..."));
+ViteExpress.bind(app, server);
