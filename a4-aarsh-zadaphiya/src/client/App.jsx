63f86e0b3191e5ff5c673a0ae339064322c91206
AarshZadaphiya <aszadaphiya@wpi.edu>
2023-10-04 12:16:49 -0400

    React Update

--- a4-aarsh-zadaphiya/src/client/App.jsx
+++ a4-aarsh-zadaphiya/src/client/App.jsx
@@ -1,34 +1,153 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+import "./index.css";
+  
+  function BmiCalculator() {
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("male");
+    const [bmi, setBmi] = useState("");
+  
+    const calculateBMI = () => {
+      const heightInMeters = height / 100;
+      const bmiValue = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+      setBmi(bmiValue);
+    };
+  
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="box">
+              <form id="bmiForm">
+                <div className="mb-3 text-start">
+                  <label className="form-label" htmlFor="height">
+                    Height (cm)
+                  </label>
+                  <input
+                    className="form-control text-start"
+                    type="number"
+                    id="height"
+                    placeholder="Height (cm)"
+                    value={height}
+                    onChange={(e) => setHeight(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-3 text-start">
+                  <label className="form-label" htmlFor="weight">
+                    Weight (kg)
+                  </label>
+                  <input
+                    className="form-control text-start"
+                    type="number"
+                    id="weight"
+                    placeholder="Weight (kg)"
+                    value={weight}
+                    onChange={(e) => setWeight(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3 text-start">
+                  <label className="form-label" htmlFor="age">
+                    Age
+                  </label>
+                  <input
+                    className="form-control text-start"
+                    type="number"
+                    id="age"
+                    placeholder="Age"
+                    value={age}
+                    onChange={(e) => setAge(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3 text-start">
+                  <label htmlFor="Gender" className="form-label">
+                    Gender
+                  </label>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="gender"
+                      id="male"
+                      value="male"
+                      checked={gender === "male"}
+                      onChange={() => setGender("male")}
+                    />
+                    <label className="form-check-label" htmlFor="male">
+                      Male
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="radio"
+                      name="gender"
+                      id="female"
+                      value="female"
+                      checked={gender === "female"}
+                      onChange={() => setGender("female")}
+                    />
+                    <label className="form-check-label" htmlFor="female">
+                      Female
+                    </label>
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <button
+                    className="btn btn-success w-100"
+                    type="button"
+                    id="Calculate"
+                    onClick={calculateBMI}
+                  >
+                    Calculate BMI
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+  
+          <div className="col-md-6 font2">
+            <div className="box d-inline-flex">
+              <div className="py-5 px-5">
+                <p>Your calculated BMI</p>
+                <div className="alert alert-success">
+                  <strong></strong> <span id="bmi">{bmi}</span>
+                </div>
+              </div>
+  
+              <div className="col-md-1"></div>
+              <div className="px-5">
+                <p>About BMI Criteria:</p>
+                <p>Underweight = &lt;18.5</p>
+                <p>Normal weight = 18.5–24.9</p>
+                <p>Overweight = 25–29.9</p>
+                <p>Obesity = BMI of 30 or greater</p>
+              </div>
+            </div>
+  
+            <div className="box">
+              <table className="table my-table">
+                <thead>
+                  <tr>
+                    <th>Date</th>
+                    <th>Height (cm)</th>
+                    <th>Weight (kg)</th>
+                    <th>Age</th>
+                    <th>Gender</th>
+                    <th>BMI</th>
+                    <th>Options</th>
+                  </tr>
+                </thead>
+                <tbody id="entryTable"></tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  );
-}
+    );
+  }
+  
 
-export default App;
+  export default BmiCalculator;
--- a4-aarsh-zadaphiya/src/server/main.js
+++ a4-aarsh-zadaphiya/src/server/main.js
@@ -1,11 +1,130 @@
-const express = require("express");
-const ViteExpress = require("vite-express");
+import express from "express";
+import { MongoClient } from "mongodb"; // Import MongoClient from the mongodb package
+import ViteExpress from "vite-express";
+import cors from "cors";
+import bcrypt from "bcrypt";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Vite + React!");
-});
+const uri =
+  "mongodb+srv://aszadaphiya:aarsh@a3-aarshzadaphiya.rnuxw22.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+let collection = null;
+let username = null;
+let password = null;
+let userData = null;
+
+const dbConnect = async function () {
+  try {
+    await client.connect();
+    collection = client.db("a3").collection("Users");
+
+    //should fetch data at every successful login
+    app.get("/docs", async (req, res) => {
+      if (collection !== null) {
+        const query = { $and: [{ username: username, password: password }] };
+        const docs = await collection.find(query).toArray();
+        userData = docs; //keep track of user data
+        res.json(docs);
+      }
+    });
+
+    app.post("/login", async (req, res) => {
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        username = req.body.username;
+        password = req.body.password;
+        //const password = hashedPassword;
+        console.log(req.body);
+        const query = { $and: [{ username: username, password: password }] };
+        const user = await collection.findOne(query);
+
+        if (user !== null) {
+          res.sendFile(__dirname + "/public/bmi.html");
+        } else {
+          res.sendFile(__dirname + "/public/index.html");
+        }
+      } catch {
+        res.status(500).send();
+      }
+    });
+
+    app.post("/delete", async (req, res) => {
+      userData[0]["data"].splice(req.body.id, 1);
+      const query = { $and: [{ username: username, password: password }] };
+      let result = await collection.replaceOne(query, userData[0]);
+      res.json(userData);
+    });
+
+    app.post("/edit", async (req, res) => {
+      const { id, height, weight, age, gender } = req.body;
+      const bmi = calculateBMI(height, weight, age, gender);
+      const oldDate = userData[0]["data"][id].date;
+      userData[0]["data"][id] = {
+        date: oldDate,
+        height: req.body.height,
+        weight: req.body.weight,
+        age: req.body.age,
+        gender: req.body.gender,
+        bmi: bmi,
+      };
+      const query = { $and: [{ username: username, password: password }] };
+      let result = await collection.replaceOne(query, userData[0]);
+      res.json(userData);
+    });
+
+    app.post("/submit", async (req, res) => {
+      try {
+        // Extract data from the request body
+        //we assume that a user already exists in the database
+        const { height, weight, age, gender } = req.body;
+        const bmi = calculateBMI(height, weight, age, gender);
+
+        //Get the array in the JSON and push the new data
+        userData[0]["data"].push({
+          date: new Date().toLocaleDateString(),
+          height: height,
+          weight: weight,
+          age: age,
+          gender: gender,
+          bmi: bmi,
+        });
+
+        const query = { $and: [{ username: username, password: password }] };
+        let result = await collection.replaceOne(query, userData[0]);
+        res.json(userData);
+      } catch (error) {
+        console.error("Error:", error);
+        res
+          .status(500)
+          .json({ success: false, message: "Internal server error" });
+      }
+    });
+
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+function calculateBMI(height, weight, age, gender) {
+  let bmi;
+  if (gender === "male") {
+    bmi = (0.74 * weight) / (((height / 100) * height) / 100);
+    bmi += 0.063 * age - 3.3;
+  } else if (gender === "female") {
+    bmi = (0.74 * weight) / (height * height);
+    bmi += 0.063 * age + 5.1;
+  } else {
+    throw new Error("Invalid gender");
+  }
+
+  return parseFloat(bmi.toFixed(2));
+}
+
+dbConnect();
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
