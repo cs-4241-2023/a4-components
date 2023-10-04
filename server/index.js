1eddd2401a2203c9c1b77da1f7d63d43ecbad0cc
arnavsacheti <cooldude012@live.com>
2023-10-04 12:07:34 -0400

    fixing heroku port

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 const app = express();
 
-const server_port = Number(process.env.SERVER_PORT) || 3000;
+const server_port = process.env.PORT || 3000;
 const express_session_token = process.env.EXPRESS_SESSION_TOKEN;
 
 app.use(express.json());
