c4cfb3bfa534c4cb2e707504ac4e933176f8c9a6
Mahir Sowad <mahirsowad3@gmail.com>
2023-10-07 14:09:33 -0400

    ported index.html UI from a2 to use react components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
 import { useState } from "react";
-import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./css/main.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const handleIncreaseCount = () => {
-    setCount(count + 1);
-  };
-
-  const handleResetCount = () => {
-    setCount(0);
-  };
-
-  const handleDecreaseCount = () => {
-    setCount(count - 1);
-  };
-
   return (
     <>
-      <h1>Test Counter</h1>
-      <h2>count: {count}</h2>
-      <button onClick={handleIncreaseCount}>Increase Count</button>
-      <button onClick={handleResetCount}>Reset Count</button>
-      <button onClick={handleDecreaseCount}>Decrease Count</button>
+      <div id="flex-container">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>Copyright &copy; 2023</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
