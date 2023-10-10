a162a043f17a6114da4698b25502a6d943546e86
da-munchies24 <donbothermeatall16@gmail.com>
2023-10-10 12:14:52 -0400

    update
    submit is not working as its getting a null value for email

--- a4/src/client/main.js
+++ a4/src/client/main.js
@@ -1,5 +1,39 @@
 import "./style.css";
 
+const submit = function (event) {
+  event.preventDefault();
+
+  let input = document.querySelectorAll("#Email, #Name, #Birth")
+   let json = {
+      Email: input[0].value,
+      Name: input[1].value,
+      Birth: input[2].value,
+      Age: age(input[2].value),
+    };
+
+  let response = fetch("/submit", {
+    method: "POST",
+    headers:{'Content-Type': 'application/json'},
+    body: JSON.stringify(json),
+  }).then(res=> res.json())
+  .then(function(res){
+    document.querySelector("#fade").style.display = "block";
+    setTimeout(function () {
+    document.querySelector("#fade").style.display = "none";
+  }, 1500);
+  })
+  display(event)
+  
+};
+
+const age= function(birth) {
+let date = new Date(birth);
+let diff = Date.now() - date.getTime();
+let date1 = new Date(diff);
+let age = Math.abs(date1.getUTCFullYear() - 1970);
+return age;
+}
+
 const display = function (event) {
   event.preventDefault();
 
@@ -17,13 +51,12 @@ const display = function (event) {
       res.forEach((d) => {
         const item = document.createElement("tr");
         item.innerHTML = `
-    <td contenteditable='false'>${d.Email}</td> 
-    <td contenteditable='false'>${d.Name}</td>
-    <td contenteditable='false'>${d.Birth}</td>
-    <td contenteditable='false'>${d.Age} years old</td>
-    <td contenteditable='false'>row:${i} <input type="checkbox" class="checkOnce" id="C${i}" 
-    onclick='checkedOnClick(this)' value='${d._id}'/></td>
-    `;
+       <td contenteditable='false'>${d.Email}</td> 
+       <td contenteditable='false'>${d.Name}</td>
+       <td contenteditable='false'>${d.Birth}</td>
+       <td contenteditable='false'>${d.Age} years old</td>
+       <td contenteditable='false'>row:${i} <input type="checkbox" class="checkOnce" id="C${i}"onclick='checkedOnClick(this)' value='${d._id}'/></td>
+       `;
         i++;
         list.appendChild(item);
       });
@@ -39,10 +72,11 @@ const clear = function (event) {
 };
 
 
-
 window.onload= function(){
+  const button1 = document.querySelector("#submit");
   const button2 = document.querySelector("#display");
   const button4 = document.querySelector("#clear");
+  button1.onclick = submit;
   button2.onclick = display;
   button4.onclick = clear;
 }
--- a4/src/server/server.js
+++ a4/src/server/server.js
@@ -8,6 +8,11 @@ app.get("/display", (req, res) => {
   res.json(appdata);
 });
 
+app.post("/submit",(req,res)=>{
+  appdata.push(req.body)
+  
+  res.json(appdata)
+})
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
 );
